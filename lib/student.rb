1e8d6c15fe321de5796d24df28ea73fd7656a72d
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-25 23:48:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-       SELECT * FROM students WHERE grade = 10 LIMIT 1
+       SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
     DB[:conn].execute(sql,x)
     self.new_from_db(x)
